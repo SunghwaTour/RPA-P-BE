2fa70c40a947a57573f13a6343409f683f734c06
6suhyeon <dbtngus20@naver.com>
2024-12-23 21:13:00 +0900

    style : change urls

--- dispatch/urls.py
+++ dispatch/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('estimate/approximate-price', views.EstimateView().as_view(), name='approximate-price'), # 견적 금액 리스트 조회회
+    path('estimates/approximate-price', views.EstimateView().as_view(), name='approximate-price'), # 견적 금액 리스트 조회회
 ]
