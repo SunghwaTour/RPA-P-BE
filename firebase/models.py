81d7638a309c48acec9ec4df7ef7bef23e6bd4de
6suhyeon <dbtngus20@naver.com>
2024-12-31 23:23:21 +0900

    fix : token duplicate update possible

--- firebase/models.py
+++ firebase/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from user.models import User
 class FCMToken(models.Model) :
     user = models.ForeignKey(User, verbose_name=("fcm_tokens"), on_delete=models.CASCADE)
-    token = models.CharField(max_length=255, unique=True)
+    token = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
